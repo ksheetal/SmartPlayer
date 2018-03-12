64b4a2055cd6bc0bfbef13a5a563291179db27dc
ksheetal <sheetal.mca17.du@gmail.com>
2018-03-12 03:25:19 -0700

    first commit

---
+++ CurrentSongHelper.kt
@@ -0,0 +1,18 @@
+package com.crypticducs000webhost.myplaylist
+
+/**
+ * Created by Hp on 12/21/2017.
+ */
+class CurrentSongHelper{
+    var songArtist:String?=null
+    var songTitle :String?=null
+    var songPath:String?=null
+    var songId:Long=0
+    var currentPostion:Int= 0
+
+    var isPlaying:Boolean = false
+    var isLoop:Boolean = false
+    var isShuffle:Boolean=false
+    var trackPostion:Int = 0
+
+}
